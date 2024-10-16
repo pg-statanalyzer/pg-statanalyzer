d9bd9ec62f8165cdaf6b19ed4165f480cadfa433
Khamidullin Rustam <rstm.khamidullin@gmail.com>
2024-10-16 22:35:28 +0700

    criteria

---
+++ src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/criteria/KolmogorovSmirnov.java
@@ -0,0 +1,23 @@
+package ru.postgrespro.perf.pgmicrobench.statanalyzer.criteria;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.stat.inference.KolmogorovSmirnovTest;
+
+public class KolmogorovSmirnov {
+    public static void main(String[] args) {
+        double[] sample = {1.2, 2.3, 2.1, 1.8, 2.5, 1.9, 2.0, 1.7};
+
+        double mean = 2.0;
+        double stdDev = 0.5;
+
+        NormalDistribution normalDistribution = new NormalDistribution(mean, stdDev);
+
+        KolmogorovSmirnovTest ksTest = new KolmogorovSmirnovTest();
+
+        double statistic = ksTest.kolmogorovSmirnovStatistic(normalDistribution, sample);
+        double pValue = ksTest.kolmogorovSmirnovTest(normalDistribution, sample);
+
+        System.out.println("Statistic: " + statistic);
+        System.out.println("p-value: " + pValue);
+    }
+}
---
+++ src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/criteria/Pearson.java
@@ -0,0 +1,35 @@
+package ru.postgrespro.perf.pgmicrobench.statanalyzer.criteria;
+
+import org.apache.commons.math3.distribution.ChiSquaredDistribution;
+
+import java.util.Arrays;
+
+public class Pearson {
+    public static double statistic(int[] a, int[] b) {
+        if (a.length != b.length) {
+            throw new IllegalArgumentException();
+        }
+        int sum = Arrays.stream(a).sum();
+
+        double statistic = 0.0;
+
+        for (int i = 0; i < a.length; i++) {
+            statistic += ((double) a[i] * a[i]) / b[i];
+        }
+
+        return statistic - sum;
+    }
+
+    public static double pValue(int[] a, int[] b, int degreesOfFreedom) {
+        double statistic = statistic(a, b);
+        ChiSquaredDistribution d = new ChiSquaredDistribution(a.length - 1 - degreesOfFreedom);
+        return 1 - d.cumulativeProbability(statistic);
+    }
+
+
+    public static void main(String[] args) {
+        double pValue = pValue(new int[]{6, 13, 38, 74, 106, 85, 30, 14}, new int[]{3, 14, 42, 82, 99, 76, 37, 13}, 2);
+
+        System.out.println("p-value: " + pValue);
+    }
+}
