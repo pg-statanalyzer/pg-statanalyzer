7fa53785f255b3017880b041a9662148d4ae9145
Rustam Khamidullin <rstm.khamidullin@gmail.com>
2024-10-30 17:32:26 +0700

    Impl Pearson, KS vs Pearson

--- src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/criteria/Pearson.java
+++ src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/criteria/Pearson.java
@@ -2,37 +2,58 @@ package ru.postgrespro.perf.pgmicrobench.statanalyzer.criteria;
 
 import org.apache.commons.math3.distribution.ChiSquaredDistribution;
 import org.apache.commons.math3.distribution.LogNormalDistribution;
-import org.apache.commons.math3.stat.inference.ChiSquareTest;
+import org.apache.commons.math3.distribution.RealDistribution;
+import org.apache.commons.math3.random.JDKRandomGenerator;
 
 import java.util.Arrays;
 
 public class Pearson {
-	public static double statistic(int[] a, int[] b) {
-		if (a.length != b.length) {
-			throw new IllegalArgumentException();
+	public static double chiSquareTest(double[] arr, RealDistribution d, int degreesOfFreedom, int bins) {
+		double p = 1. / bins;
+		double pSquare = p * p;
+		int N = arr.length;
+
+		double[] bounds = boundsOfBins(arr, bins);
+
+		double[] tP = new double[bins];
+		double prev = 0;
+		for (int i = 0; i < bins - 1; i++) {
+			double cur = d.cumulativeProbability(bounds[i]);
+			tP[i] = cur - prev;
+			prev = cur;
 		}
-		int sum = Arrays.stream(a).sum();
+		tP[bins - 1] = 1 - prev;
 
-		double statistic = 0.0;
 
-		for (int i = 0; i < a.length; i++) {
-			statistic += ((double) a[i] * a[i]) / b[i];
+		double statistic = 0;
+		for (int i = 0; i < bins; i++) {
+			statistic += pSquare / tP[i];
+		}
+		statistic -= 1;
+		statistic *= N;
+
+
+		ChiSquaredDistribution chiSquaredDistribution = new ChiSquaredDistribution(bins - 1 - degreesOfFreedom);
+		return 1 - chiSquaredDistribution.cumulativeProbability(statistic);
+	}
+
+
+	private static double[] boundsOfBins(double[] arr, int bins) {
+		int N = arr.length;
+
+		double[] sorted = Arrays.stream(arr).sorted().toArray();
+
+		double[] bounds = new double[bins - 1];
+
+		int amount = N / bins;
+		int rem = N - amount * bins;
+		int curPos = 0;
+
+		for (int i = 0; i < bounds.length; i++) {
+			curPos += amount + (i < rem ? 1 : 0);
+			bounds[i] = sorted[curPos - 1];
 		}
 
-		return statistic - sum;
+		return bounds;
 	}
-
-	public static double pValue(int[] a, int[] b, int degreesOfFreedom) {
-		double statistic = statistic(a, b);
-		ChiSquaredDistribution d = new ChiSquaredDistribution(a.length - 1 - degreesOfFreedom);
-		return 1 - d.cumulativeProbability(statistic);
-	}
-
-
-	public static void main(String[] args) {
-		double pValue = pValue(new int[]{6, 13, 38, 74, 106, 85, 30, 14}, new int[]{3, 14, 42, 82, 99, 76, 37, 13}, 2);
-
-		System.out.println("p-value: " + pValue);
-	}
-
 }
--- src/main/test/criteria/TestKSvsPearson.java
+++ src/main/test/criteria/TestKSvsPearson.java
@@ -3,17 +3,19 @@ package criteria;
 import org.apache.commons.math3.distribution.RealDistribution;
 import org.apache.commons.math3.stat.inference.KolmogorovSmirnovTest;
 import org.junit.jupiter.api.Test;
+import ru.postgrespro.perf.pgmicrobench.statanalyzer.criteria.Pearson;
 import util.parsing.DistributionParser;
 import util.parsing.ParsedDistribution;
 
 import java.io.FileNotFoundException;
 
-public class TestKolmogorovSmirnov {
+public class TestKSvsPearson {
 	@Test
-	public void testKolmogorovSmirnov() throws FileNotFoundException {
+	public void testKSvsPearson() throws FileNotFoundException {
 		String[] files = new String[] {
 				"lognorm_13_07",
 				"lognorm_4278_001",
+				"lognorm_3_0.3",
 				"gamma_10_03"
 		};
 
@@ -27,8 +29,12 @@ public class TestKolmogorovSmirnov {
 			KolmogorovSmirnovTest test = new KolmogorovSmirnovTest();
 
 			double pValue = test.kolmogorovSmirnovTest(distribution, data);
+			double pearsonPValue = Pearson.chiSquareTest(data, distribution, 2, 50);
 
-			System.out.println(file + " pValue=" + pValue);
+			System.out.println(file + " " + (pValue + " " + pearsonPValue).replace('.', ','));
+
+
+			System.out.println();
 		}
 	}
 }
