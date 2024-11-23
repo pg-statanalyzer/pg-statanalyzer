cd9db5268399f57c4d24871f05bdb14493c38628
d.barsukova <d.barsukova@g.nsu.ru>
2024-11-23 20:54:06 +0700

    delete

--- src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/StatAnalyzer.java
+++ src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/StatAnalyzer.java
@@ -5,7 +5,6 @@ import ru.postgrespro.perf.pgmicrobench.statanalyzer.distributions.recognition.F
 import ru.postgrespro.perf.pgmicrobench.statanalyzer.distributions.recognition.KolmogorovSmirnov;
 import ru.postgrespro.perf.pgmicrobench.statanalyzer.multimodality.LowlandModalityDetector;
 import ru.postgrespro.perf.pgmicrobench.statanalyzer.multimodality.ModalityData;
-import ru.postgrespro.perf.pgmicrobench.statanalyzer.multimodality.RangedMode;
 import ru.postgrespro.perf.pgmicrobench.statanalyzer.plotting.Plot;
 
 import java.io.File;
@@ -14,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 /**
  * Application class.
