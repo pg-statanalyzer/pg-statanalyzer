b70717db055dd18112998e451dcaa4665e9a7dc0
Daria <115018770+daria-barsukova@users.noreply.github.com>
2024-12-18 13:59:48 +0700

    tests (#17)
    * add simple test and gamma type
    
    * fix build
    
    * test
    
    * build and checkstyle
    
    * checkstyle
    
    * checkstyle

--- src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/distributions/PgDistributionType.java
+++ src/main/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/distributions/PgDistributionType.java
@@ -7,11 +7,12 @@ import java.util.function.Function;
 
 @RequiredArgsConstructor
 public enum PgDistributionType {
-    NORMAL(2, params -> new PgNormalDistribution(params[0], params[1]), new double[] {1, 1}),
-    LOGNORMAL(2, params -> new PgLogNormalDistribution(params[0], params[1]), new double[] {1, 0.5}),
-    GUMBEL(2, params -> new PgGumbelDistribution(params[0], params[1]), new double[] {1, 1}),
-//    UNIFORM(2, params -> new PgUniformDistribution(params[0], params[1]), new double[] {0, 1}),
-    WEIBULL(2, params -> new PgWeibullDistribution(params[0], params[1]), new double[] {1, 1});
+    NORMAL(2, params -> new PgNormalDistribution(params[0], params[1]), new double[]{1, 1}),
+    LOGNORMAL(2, params -> new PgLogNormalDistribution(params[0], params[1]), new double[]{1, 0.5}),
+    GUMBEL(2, params -> new PgGumbelDistribution(params[0], params[1]), new double[]{1, 1}),
+    //    UNIFORM(2, params -> new PgUniformDistribution(params[0], params[1]), new double[] {0, 1}),
+    WEIBULL(2, params -> new PgWeibullDistribution(params[0], params[1]), new double[]{1, 1});
+    //GAMMA(2, params -> new PgGammaDistribution(params[0], params[1]), new double[] {1, 1});
 
     @Getter
     private final int parameterNumber;
---
+++ src/test/java/ru/postgrespro/perf/pgmicrobench/statanalyzer/tests/statanalyzer/StatAnalyzerTest.java
@@ -0,0 +1,112 @@
+package ru.postgrespro.perf.pgmicrobench.statanalyzer.tests.statanalyzer;
+
+import ru.postgrespro.perf.pgmicrobench.statanalyzer.AnalysisResult;
+import ru.postgrespro.perf.pgmicrobench.statanalyzer.ModeReport;
+import ru.postgrespro.perf.pgmicrobench.statanalyzer.StatAnalyzer;
+import ru.postgrespro.perf.pgmicrobench.statanalyzer.distributions.PgDistributionType;
+import ru.postgrespro.perf.pgmicrobench.statanalyzer.distributions.recognition.FittedDistribution;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static ru.postgrespro.perf.pgmicrobench.statanalyzer.distributions.PgDistributionType.*;
+
+/**
+ * Test.
+ */
+public class StatAnalyzerTest {
+
+    private static final String TEST_FILE = "sampleGenerator/multi_for_test.txt";
+    private static final double[][] EXPECTED_PARAMS = {
+            {1.0, 1.7},  // weibull
+            {8.88, 1.1}, // normal
+            {3.15, 0.07} // lognormal
+    };
+
+    private static final PgDistributionType[] DISTRIBUTION_ORDER = {WEIBULL, NORMAL, LOGNORMAL};
+
+    @Test
+    public void testStatAnalyzerWithGeneratedData() throws IOException, InterruptedException {
+        //generateTestData();
+
+        List<Double> dataList = readSampleFromFile(TEST_FILE);
+
+        //Sample sample = new Sample(dataList, true);
+        //Plot.plot(sample);
+
+        StatAnalyzer statAnalyzer = new StatAnalyzer();
+        AnalysisResult analysisResult = statAnalyzer.analyze(dataList);
+        //Plot.plot(new Sample(dataList), analysisResult.getPdf(), "Summary pdf");
+        Thread.sleep(2000);
+
+        int expectedModes = EXPECTED_PARAMS.length;
+        assertEquals(expectedModes, analysisResult.getModeReports().size(), "Number of modes not as expected");
+
+        for (int i = 0; i < expectedModes; i++) {
+            ModeReport modeReport = analysisResult.getModeReports().get(i);
+            FittedDistribution bestDistribution = modeReport.getBestDistribution();
+
+            assertEquals(DISTRIBUTION_ORDER[i], bestDistribution.getType(),
+                    "Not correct distribution " + (i + 1));
+
+            double[] expectedParams = EXPECTED_PARAMS[i];
+            double[] actualParams = bestDistribution.getParameters();
+
+            assertNotNull(actualParams, "Params should not be null");
+
+            assertEquals(expectedParams[0], actualParams[0], 0.2,
+                    "First param not correct " + DISTRIBUTION_ORDER[i]);
+            assertEquals(expectedParams[1], actualParams[1], 0.2,
+                    "Second param not correct " + DISTRIBUTION_ORDER[i]);
+        }
+
+        System.out.println("Result: ");
+        analysisResult.getModeReports().forEach(System.out::println);
+    }
+
+    private void generateTestData() {
+        try {
+            System.out.println("Запуск Python-генератора данных...");
+            ProcessBuilder processBuilder = new ProcessBuilder("python3", "sampleGenerator/sampleGenerator.py");
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("Error while processing generator " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Problem with generator", e);
+        }
+    }
+
+    private List<Double> readSampleFromFile(String filePath) throws IOException {
+        List<Double> data = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replace(",", ".").trim();
+                if (line.isEmpty() || !Character.isDigit(line.charAt(0))) {
+                    continue;
+                }
+                data.add(Double.parseDouble(line));
+            }
+        }
+        return data;
+    }
+}
